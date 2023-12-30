33a473408d9f6dc871d66b99a74fe2df5938efce
CB\20395 <Osman.Tuysuz@sekerbank.com.tr>
2023-12-30 11:27:33 +0300

    Basic Pricing added.

---
+++ frontendDaily/12- Basic Pricing/app.js
@@ -0,0 +1,12 @@
+function circleBar(elemId, percent) {
+  let elem = document.getElementById(elemId);
+  let circles = elem.querySelectorAll("circle");
+  let r = circles[0].getAttribute("r");
+  let circum = 2 * Math.PI * r;
+  let offset = circum - (circum * percent) / 100;
+
+  circles[1].style.setProperty("--offset", offset);
+  circles[1].style.setProperty("--circum", circum);
+}
+
+circleBar("elem1", 60);
